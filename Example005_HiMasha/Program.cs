51facb804eaf5a0667d73953f9893e9886d9a9ba
Dima <dima.brashovyan@mail.ru>
2022-04-27 00:27:04 +0300

    Да это же Ксюша

---
+++ Example005_HiMasha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+String username = Console.ReadLine();
+
+if(username.ToLower() == "ксюша")
+{
+    Console.WriteLine("Ура, это же КСЮША!");
+}
+else
+{
+    Console.Write("Приет, ");
+    Console.WriteLine(username);
+}
